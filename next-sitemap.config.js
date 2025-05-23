463a87ce8fa9bf3e8f85eff57b55762d6aa5454f
Aarifansarikht <arifansarikhtz@gmail.com>
2025-05-23 15:34:41 +0530

    update

---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+/** @type {import('next-sitemap').IConfig} */
+export const siteUrl = 'https://adlexinternational.vercel.app/';
+export const generateRobotsTxt = true;
+export const sitemapSize = 5000;
