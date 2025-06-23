82ceab7c927d732c322d1bdccbabd23a255cf363
Aarifansarikht <arifansarikhtz@gmail.com>
2025-06-23 17:43:49 +0530

    update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,21 +1,33 @@
-import SafeAreaTop from "@/layout/SafeAreaTop";
 import React from "react";
+import Head from "next/head";
+import SafeAreaTop from "@/layout/SafeAreaTop";
 import ContactFormSection from "./ContactFormSection";
 import Breadscrumb from "@/components/breadscrumb/Breadscrumb";
 
 export default function page() {
   return (
-    <div>
+    <>
+      <Head>
+        <title>Contact Us | Adlex International</title>
+        <meta
+          name="description"
+          content="Get in touch with Adlex International for sustainable waste management and recycling services. Contact us today!"
+        />
+        <link
+          rel="canonical"
+          href="https://www.adlexinternational.com/contact"
+        />
+      </Head>
+
       <SafeAreaTop>
         <Breadscrumb
           items={[
             { label: "Home", href: "/" },
-            { label: "Conatct Us", href: "/contact" },
+            { label: "Contact Us", href: "/contact" },
           ]}
         />
-
         <ContactFormSection />
       </SafeAreaTop>
-    </div>
+    </>
   );
 }
